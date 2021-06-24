835a057d2e04ea9b16c8827619edd3cac380af35
kaleaniket-official <84848685+kaleaniket-official@users.noreply.github.com>
2021-06-24 17:04:56 +0530

    Updated version and added API to set Aadhar verification

--- Routers/ApiDashboard.js
+++ Routers/ApiDashboard.js
@@ -4,10 +4,11 @@ const loginService = require('../Service/LoginService');
 const tenantService = require('../Service/TenantService');
 
 
-router.post('/login', loginService.login);
-router.get('/getTenants', loginService.auth, tenantService.getTenants);
-router.post('/createTenant', tenantService.createTenant);
-router.get('/getTenantByUserName/:userName', loginService.auth, tenantService.getTenantByUserName);
-router.get('/getTenantById/:id', loginService.auth, tenantService.getTenantById)
+router.post('/v1/login', loginService.login);
+router.get('/v1/getTenants', loginService.auth, tenantService.getTenants);
+router.post('/v1/createTenant', tenantService.createTenant);
+router.get('/v1/getTenantByUserName/:userName', loginService.auth, tenantService.getTenantByUserName);
+router.get('/v1/getTenantById/:id', loginService.auth, tenantService.getTenantById)
+router.put('/v1/setIsAadharVerified', loginService.auth, tenantService.setIsAadharVerified)
 
 module.exports = router;
--- Service/TenantService.js
+++ Service/TenantService.js
@@ -6,11 +6,12 @@ var createTenant = async (req,res) => {
         const {tenantUserName, tenantName, email, phoneNumber, aadharNumber, password} = req.body;
 
         const insertQuery = "INSERT into public.tenant(tenantUserName, tenantName, email, "
-            +"phoneNumber, aadharNumber, password, created_date, updated_date) values($1, $2, $3, $4, $5, crypt($6, gen_salt('md5')), now(), now())";
+            +"phoneNumber, password, created_date, updated_date) values($1, $2, $3, $4, crypt($6, gen_salt('md5')), now(), now())";
         const data = await connectDB.query(insertQuery, [tenantUserName, tenantName, email, phoneNumber, aadharNumber, password]);
         res.status(200).send({message: "tenant created!!"});
     }catch(err){
         console.error(err.message);
+        res.status(500).send({message: "Failed to create tenant."});
     }
 }
 
@@ -21,6 +22,7 @@ var getTenants = async (req,res) => {
         res.send(data.rows);
     }catch(err){
         console.error(err.message);
+        res.status(500).send({message: "Failed to fetch tenants."});
     }
 }
 
@@ -28,9 +30,15 @@ var getTenantById = async (req,res) => {
     try{
         const id = req.params.id;
         const data = await connectDB.query("SELECT * from public.tenant where tenantId = $1",[id]);
-        res.send(data.rows);
+        if(data.rows && data.rows.length > 0){
+            res.send(data.rows[0]);
+        }
+        else{
+            res.status(404).send({message: "Tenant not present with ID: "+id});
+        }
     }catch(err){
         console.error(err.message);
+        res.status(500).send({message: "Failed to get tenant by ID."});
     }
 }
 
@@ -38,14 +46,40 @@ var getTenantByUserName = async (req,res) => {
     try{
         const userName = req.params.userName;
         const data = await connectDB.query("SELECT * from public.tenant where tenantUserName = $1",[userName]);
-        res.send(data.rows[0]);
+        if(data.rows && data.rows.length > 0){
+            res.send(data.rows[0]);
+        }
+        else{
+            res.status(404).send({message: "Tenant not present with username: "+userName});
+        }
     }catch(err){
         console.error(err.message);
+        res.status(500).send({message: "Failed to get tenant by username."});
+
+    }
+}
+
+var setIsAadharVerified = async (req,res) => {
+    try{
+        const isAadharVerified = req.body.isAadharVerified;
+        const tenantId = req.body.tenantId;
+        const data = await connectDB.query("UPDATE public.tenant SET isaadharverified = $1 where tenantid= $2",[isAadharVerified,tenantId]);
+        if(data.rowCount && data.rowCount > 0){
+            res.status(200).send({message: `Updated Tenant with ID ${tenantId}, set aadhar verified to: ${isAadharVerified}`}) ;
+        }
+        else{
+            res.status(404).send({message: "Tenant not present with id: "+tenantId});
+        }
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send({message: "Failed to update tenant with ID: "+tenantId});
+
     }
 }
 module.exports = {
      getTenants: getTenants,
      getTenantById: getTenantById,
      getTenantByUserName: getTenantByUserName,
-     createTenant: createTenant
+     createTenant: createTenant,
+     setIsAadharVerified: setIsAadharVerified
 }
